a84a072a74891a1aca5aaff5f4172d9e5192dee5
M1hajlo07 <mihajlonaumoski07@gmail.com>
2024-12-31 18:07:12 +0100

    DomasnaZadacabr6

--- src/app/comp/comp.component.ts
+++ src/app/comp/comp.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from '@angular/core';
 import { driver } from '../../driver';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-comp',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './comp.component.html',
   styleUrl: './comp.component.css'
 })
@@ -13,6 +14,27 @@ vozac:driver | undefined;
 @Input()
 indks:number | undefined;
 onDrView(){
-  console.log("KLIKNA ME");
+  let link:string | undefined;
+  if(this.vozac?.iconUrl){
+    link=this.vozac?.iconUrl
+  }
+  else{
+    link="https://www.google.com"
+  };
+  window.open(link,"_blank")
+}
+
+klasi(){
+  return {'begin':this.vozac?.category=='ASD',
+    'adv':this.vozac?.category=='EXPERT',
+    }
+  }
+klasi2(){
+  if(this.vozac?.category=='ASD'){return 'begin'}
+  else{return 'adv'}
+}
+stilovi(){
+  return{'undr':this.vozac?.category=='EXPERT'}
 }
 }
+
